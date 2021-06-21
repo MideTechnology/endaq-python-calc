d75713b7975e1de8aff1517decb8a582a21754f7
MIDE\cflanigan <cflanigan@mide.com>
2021-06-21 15:45:36 -0400

    initializing and updating readme, license, requirements, and setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open('README.rst', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = [
+    ]
+
+TEST_REQUIRES = [
+    ]
+
+EXAMPLE_REQUIRES = [
+    ]
+
+setuptools.setup(
+        name='endaq-python-calc',
+        version='1.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        description='A comprehensive, user-centric Python API for working with enDAQ data and devices',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/MideTechnology/endaq-python',
+        license='MIT',
+        classifiers=['Development Status :: 2 - Pre-Alpha',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
+                     'Topic :: Scientific/Engineering',
+                     ],
+        keywords='ebml binary ide mide endaq',
+        packages=['endaq.calc'],
+        package_dir={'endaq.calc': './endaq/calc'},
+        install_requires=INSTALL_REQUIRES,
+        extras_require={
+            'test': INSTALL_REQUIRES + TEST_REQUIRES,
+            'example': INSTALL_REQUIRES + EXAMPLE_REQUIRES,
+            },
+)
