8ae95b9b92d0c5b3bc8798280efc6bdb7dcf2684
Becker A <bawqatty@mide.com>
2021-08-16 18:39:14 -0400

    added to PSD octave functions (#3)

--- endaq/calc/psd.py
+++ endaq/calc/psd.py
@@ -1,7 +1,11 @@
+from collections import namedtuple
 import warnings
 
 import numpy as np
 import scipy.signal
+
+
+SpectrumStruct = namedtuple("SpectrumStruct", "freqs, values")
 
 
 def _np_histogram_nd(array, bins=10, weights=None, axis=-1, **kwargs):
@@ -64,10 +68,10 @@ def differentiate(f, psd, n=1):
     if n < 0:
         factor[f == 0] = 0
 
-    return f, psd * factor
+    return SpectrumStruct(freqs=f, values=psd * factor)
 
 
-def to_jagged(f, psd, freq_splits, axis=-1, mode="sum"):
+def to_jagged(f, psd, freq_splits, axis=-1, agg="sum"):
     """
     Calculate a periodogram over non-uniformly spaced frequency bins.
 
@@ -75,10 +79,11 @@ def to_jagged(f, psd, freq_splits, axis=-1, mode="sum"):
     :param freq_splits: the boundaries of the frequency bins; must be strictly
         increasing
     :param axis: same as the axis parameter provided to `scipy.signal.welch`
-    :param mode: the method for aggregating values into bins; 'mean' preserves
+    :param agg: the method for aggregating values into bins; 'mean' preserves
         the PSD's area-under-the-curve, 'sum' preserves the PSD's "energy"
     """
-    if not np.all(np.diff(freq_splits, prepend=0) > 0):
+    f = np.asarray(f)
+    if f.ndim != 1 or not np.all(np.diff(freq_splits, prepend=0) > 0):
         raise ValueError
 
     # Check that PSD samples do not skip any frequency bins
@@ -90,17 +95,38 @@ def to_jagged(f, psd, freq_splits, axis=-1, mode="sum"):
             RuntimeWarning,
         )
 
-    psd_jagged = _np_histogram_nd(f, bins=freq_splits, weights=psd)
-    if mode == "mean":
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=RuntimeWarning)  # x/0
+    if isinstance(agg, str):
+        if agg not in ("sum", "mean"):
+            raise ValueError(f'invalid aggregation mode "{agg}"')
 
-            psd_jagged = np.nan_to_num(  # <- fix divisions by zero
-                psd_jagged / np.histogram(f, bins=freq_splits)[0]
-            )
+        # Reshape frequencies for histogram function
+        f_ndim = np.broadcast_to(
+            f,
+            (psd.shape[:axis] + psd.shape[axis + 1 or psd.ndim :] + (psd.shape[axis],)),
+        )
+        f_ndim = np.moveaxis(f_ndim, -1, axis)
+
+        # Calculate sum via histogram function
+        psd_jagged = _np_histogram_nd(f_ndim, bins=freq_splits, weights=psd, axis=axis)
+
+        # Adjust values for mean calculation
+        if agg == "mean":
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=RuntimeWarning)  # x/0
+
+                psd_jagged = np.moveaxis(psd_jagged, axis, -1)
+                psd_jagged = np.nan_to_num(  # <- fix divisions by zero
+                    psd_jagged / np.histogram(f, bins=freq_splits)[0]
+                )
+                psd_jagged = np.moveaxis(psd_jagged, -1, axis)
+
+    else:
+        psd_binned = np.split(psd, np.searchsorted(f, freq_splits), axis=axis)[1:-1]
+        psd_jagged = np.stack([agg(a, axis=axis) for a in psd_binned], axis=axis)
 
     f = (freq_splits[1:] + freq_splits[:-1]) / 2
-    return f, psd_jagged
+
+    return SpectrumStruct(freqs=f, values=psd_jagged)
 
 
 def to_octave(f, psd, fstart=1, octave_bins=12, **kwargs):
@@ -120,4 +146,56 @@ def to_octave(f, psd, fstart=1, octave_bins=12, **kwargs):
     )
     assert len(center_freqs) + 1 == len(freq_splits)
 
-    return center_freqs, to_jagged(f, psd, freq_splits=freq_splits, **kwargs)[1]
+    return SpectrumStruct(
+        freqs=center_freqs,
+        values=to_jagged(f, psd, freq_splits=freq_splits, **kwargs).values,
+    )
+
+
+def vc_curves(f, psd, fstart=1, octave_bins=12, axis=-1):
+    """
+    Calculate Vibration Criterion (VC) curves from an acceration periodogram.
+
+    # Theory behind the calculation
+
+    Let x(t) be a real-valued time-domain signal, and X(2πf) = F{x(t)}(2πf)
+    be the Fourier Transform of that signal. By Parseval's Theorem,
+
+        ∫x(t)^2 dt = ∫|X(2πf)|^2 df
+
+    (see https://en.wikipedia.org/wiki/Parseval%27s_theorem#Notation_used_in_physics)
+
+    Rewriting the right side of that equation in the discrete form becomes
+
+        ∫x(t)^2 dt ≈ ∑ |X[k]|^2 • ∆f
+
+    where ∆f = fs/N = (1/∆t) / N = 1/T.
+    Limiting the right side to a range of discrete frequencies (k_0, k_1):
+
+        ∫x(t)^2 dt ≈ [∑; k=k_0 -> k≤k_1] |X[k]|^2 • ∆f
+
+    The VC curve calculation is the RMS over the time-domain. If T is the
+    duration of the time-domain signal, then:
+
+        √((1/T) ∫x(t)^2 dt)
+            ≈ √((1/T) [∑; k=k_0 -> k≤k_1] |X[k]|^2 • ∆f)
+            = ∆f • √([∑; k=k_0 -> k≤k_1] |X[k]|^2)
+
+    If the time-series data is acceleration, then the signal needs to first
+    be integrated into velocity. This can be done in the frequency domain
+    by replacing |X(2πf)|^2 with (1/2πf)^2 |X(2πf)|^2.
+    """
+    f, v_psd = differentiate(f, psd, n=-1)
+    f_oct, v_psd_oct = to_octave(
+        f,
+        v_psd,
+        fstart=fstart,  # Hz
+        octave_bins=octave_bins,
+        agg=np.sum,
+        axis=axis,
+    )
+
+    return SpectrumStruct(
+        freqs=f_oct,
+        values=np.sqrt(f[1] * v_psd_oct),  # the PSD must already scale by ∆f?
+    )
---
+++ tests/calc/test_psd.py
@@ -0,0 +1,86 @@
+import timeit
+
+import pytest
+import hypothesis as hyp
+import hypothesis.strategies as hyp_st
+import hypothesis.extra.numpy as hyp_np
+import numpy as np
+
+from endaq.calc import psd
+
+
+@hyp.given(
+    psd_array=hyp_np.arrays(
+        dtype=np.float64,
+        shape=(20, 3),
+        elements=hyp_st.floats(
+            0,
+            1e20,
+            allow_nan=False,
+            allow_infinity=False,
+        ),
+    ),
+    freq_diffs=hyp_np.arrays(
+        dtype=np.float64,
+        shape=(8,),
+        elements=hyp_st.floats(
+            0,
+            20,
+            allow_nan=False,
+            allow_infinity=False,
+            exclude_min=True,
+        ),
+    ),
+)
+@pytest.mark.parametrize(
+    "agg1, agg2",
+    [
+        ("mean", lambda x, axis=-1: np.nan_to_num(np.mean(x, axis=axis))),
+        ("sum", np.sum),
+    ],
+)
+def test_to_jagged_modes(psd_array, freq_diffs, agg1, agg2):
+    """Test `to_jagged(..., mode='mean')` against the equivalent `mode=np.mean`."""
+    axis = 0
+    f = np.arange(psd_array.shape[axis]) * 10
+    freq_splits = np.cumsum(freq_diffs)
+    hyp.assume(np.all(np.diff(freq_splits, prepend=0) > 0))
+
+    result1 = psd.to_jagged(f, psd_array, freq_splits, axis=axis, agg=agg1).values
+    result2 = psd.to_jagged(f, psd_array, freq_splits, axis=axis, agg=agg2).values
+
+    np.testing.assert_allclose(
+        result1,
+        result2,
+        atol=np.amin(psd_array * 1e-7),
+    )
+
+
+def test_to_jagged_mode_times():
+    setup = """
+from endaq.calc import psd
+import numpy as np
+
+n = 10 ** 4
+
+axis = -1
+psd_array = np.random.random((3, n))
+f = np.arange(n) / 3
+#freq_splits = np.logspace(0, np.log2(n), num=100, base=2)
+freq_splits = f[1:-1]
+    """
+
+    t_direct = timeit.timeit(
+        "psd.to_jagged(f, psd_array, freq_splits, axis=axis, agg=np.sum)",
+        setup=setup,
+        number=3,
+    )
+    t_hist = timeit.timeit(
+        "psd.to_jagged(f, psd_array, freq_splits, axis=axis, agg='sum')",
+        setup=setup,
+        number=3,
+    )
+
+    print(f"direct form time: {t_direct}")
+    print(f"histogram time: {t_hist}")
+    assert t_hist < t_direct
