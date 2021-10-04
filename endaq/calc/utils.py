9b4d3d9cf0af749e8a85b81de21071a0719abf7d
Becker Awqatty <bawqatty@mide.com>
2021-10-04 10:01:27 -0400

    added utility function `logfreqs`

---
+++ endaq/calc/utils.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Optional
+import warnings
+
+import numpy as np
+import pandas as pd
+
+
+def logfreqs(
+    df: pd.DateTime, init_freq: Optional[float] = None, bins_per_octave: float = 12
+) -> np.ndarray:
+    """
+    Calculate a sequence of log-spaced frequencies for a given dataframe.
+
+    :param df: the input data
+    :param init_freq: the initial frequency in the sequence; if None (default),
+        use the frequency corresponding to the data's duration
+    :param bins_per_octave: the number of frequencies per octave
+    :return: an array of log-spaced frequencies
+    """
+    dt = (df.index[-1] - df.index[0]) / (len(df.index) - 1)
+    if isinstance(dt, (np.timedelta64, pd.Timedelta)):
+        dt = dt / np.timedelta64(1, "s")
+
+    T = dt * len(df.index)
+    if init_freq is None:
+        init_freq = 1 / T
+    elif 1 / init_freq > T:
+        warnings.warn(
+            "the data's duration is too short to accurately represent an"
+            f" initial frequency of {init_freq:.3f} Hz",
+            RuntimeWarning,
+        )
+
+    return 2 ** np.arange(
+        np.log2(init_freq),
+        np.log2(1 / dt) - 1,
+        1 / bins_per_octave,
+    )
